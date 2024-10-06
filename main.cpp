c10c0e34bd719198c6f308cb7578ef8556b3c3af
Trisna22 <trisna@live.nl>
2024-10-06 22:24:25 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -10,24 +10,34 @@ int main(int argc, char* argv[]) {
 
 	// Implement an option parser for port/authentication/proxy type
 
-	printf("[#] Starting proxy server on port 1080!\n\n");
-
 	int serverSocket;
-	if (argc == 1) {
-		if ((serverSocket = Sock5Proxy::startProxy()) == SOCKET_ERROR) {
+	if (argc == 2) {
+
+		int port = atoi(argv[1]);
+		if (port == 0 || port == -1) {
+
+			printf("Invalid port given!\n");
+			printf("Usage: %s [port number]\n", argv[0]);
 			return 0;
 		}
-	}
-	else if (argc == 2) {
-		int port = atoi(argv[1]);
+
+		printf("Starting SOCK5 proxy on port %d\n", port);
+
 		if ((serverSocket = Sock5Proxy::startProxy(port)) == SOCKET_ERROR) {
 			return 0;
 		}
 	}
-	else {
-		printf("Sock5 Proxy\n\n");
-		printf("Usage: %s [port number]\n\n", argv[0]);
+	else if (argc > 2) {
+
+		printf("Usage: %s [port number]\n", argv[0]);
 		return 0;
+	}
+	else {
+
+		printf("Starting SOCK5 proxy on port 1080\n");
+		if ((serverSocket = Sock5Proxy::startProxy()) == SOCKET_ERROR) {
+			return 0;
+		}
 	}
 
 	return Sock5Proxy::handleClients(serverSocket);
